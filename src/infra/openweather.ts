8ee68f454de1708a3cd3c46f45a13d4a03e512cc
agrippa <afsalahamed07@gmail.com>
2025-03-15 12:56:39 +0530

    Changing the defult kelvin temp unit to celcius.

--- src/infra/openweather.ts
+++ src/infra/openweather.ts
@@ -7,7 +7,7 @@ const openWeatherApiKey = process.env.OPEN_WEATHER_API_KEY as string;
 export async function getWeatherByCity(city: string) {
   console.log(openWeatherApiKey);
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}`,
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}&units=metric`,
   );
   const data = await response.json();
   return data;
