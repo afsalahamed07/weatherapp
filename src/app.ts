963ccd75a2463a6a533b356252d60281ed292b19
agrippa <afsalahamed07@gmail.com>
2025-03-16 19:13:37 +0530

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,8 @@
 import express from "express";
 import userRouter from "./routes/userRoute.js";
 import connectDB from "./config/db.js";
-import User from "./models/User.js";
-import { sendMail } from "./infra/mailer.js";
-import { getWeatherByCity } from "./infra/openweather.js";
-import { generateText } from "./infra/gemini.js";
 import passport from "./config/passport.js";
-import { getCityFromCoordinates } from "./infra/googleGeocode.js";
+import { sendWeatherUpdate } from "./infra/weatherUtil.js";
 
 const app = express();
 
@@ -28,53 +24,11 @@ app.use("/users", userRouter);
 // Mongoose DB Connection
 connectDB();
 
-const PORT = 3000;
-
-async function sendWeatherUpdate() {
-  const users = await User.find();
-
-  for (let user of users) {
-    let city = null;
-    if (user.location) {
-      city = user.location;
-      console.log(`User ${user.name} has a city: ${city}`);
-    } else if (user.lat && user.lon) {
-      city = await getCityFromCoordinates(user.lat, user.lon);
-    }
-    if (city != null) {
-      console.log(`Getting weather for ${city}`);
-      try {
-        const weather = await getWeatherByCity(city);
-        console.log(`Weather for ${city}: ${weather.weather[0].description}`);
-        const prompt = `The current weather in ${weather.name} is ${weather.weather[0].description} with a temperature of ${weather.main.temp}°C. Compose a humorous message regarding the weather. Incorporate the weather data in you message aswell`;
-        const message = await generateText(prompt);
-        // const message = "message here";
-
-        const emailSubject = `Weather Update for ${user.location}`;
-        const emailBody = `Ola ${user.name}!
-
-              Here is your weather update:
-              ${message}
-
-              Stay prepared and have a great day!
-
-              Regards,
-              Your Weather App`;
-
-        sendMail(user.email, emailSubject, emailBody);
-      } catch (error) {
-        console.error(`Failed to get weather for ${city}`);
-        console.error(error);
-      }
-    } else {
-      console.log(`User ${user.name} does not have a city`);
-    }
-  }
-}
-
 sendWeatherUpdate();
 
 setInterval(sendWeatherUpdate, 3 * 1000 * 60 * 60);
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Weather app listening on port ${PORT}!`);
---
+++ src/infra/weatherUtil.ts
@@ -0,0 +1,48 @@
+import User from "../models/User.js";
+import { getWeatherByCity } from "./openweather.js";
+import { getCityFromCoordinates } from "./googleGeocode.js";
+import { sendMail } from "./mailer.js";
+import { generateText } from "./gemini.js";
+
+export async function sendWeatherUpdate() {
+  const users = await User.find();
+
+  for (let user of users) {
+    let city = null;
+    if (user.location) {
+      city = user.location;
+      console.log(`User ${user.name} has a city: ${city}`);
+    } else if (user.lat && user.lon) {
+      // the lat and lon can be directly used with the openweather api.
+      // the google geocoding is only used due to the assignment requirements.
+      city = await getCityFromCoordinates(user.lat, user.lon);
+    }
+    if (city != null) {
+      console.log(`Getting weather for ${city}`);
+      try {
+        const weather = await getWeatherByCity(city);
+        console.log(`Weather for ${city}: ${weather.weather[0].description}`);
+        const prompt = `The current weather in ${weather.name} is ${weather.weather[0].description} with a temperature of ${weather.main.temp}°C. Compose a humorous message regarding the weather. Incorporate the weather data in you message aswell`;
+        const message = await generateText(prompt);
+
+        const emailSubject = `Weather Update for ${user.location}`;
+        const emailBody = `Ola ${user.name}!
+
+              Here is your weather update:
+              ${message}
+
+              Stay prepared and have a great day!
+
+              Regards,
+              Your Weather App`;
+
+        sendMail(user.email, emailSubject, emailBody);
+      } catch (error) {
+        console.error(`Failed to get weather for ${city}`);
+        console.error(error);
+      }
+    } else {
+      console.log(`User ${user.name} does not have a city`);
+    }
+  }
+}
